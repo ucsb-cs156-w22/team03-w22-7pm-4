9d9c61a644f0d17e81f076b9fb9cd631ca572d07
allisonhuang2 <72825117+allisonhuang2@users.noreply.github.com>
2022-02-17 13:56:08 -0800

    change dropdown menu from list earthquakes to just list

--- frontend/src/main/components/Nav/AppNavbar.js
+++ frontend/src/main/components/Nav/AppNavbar.js
@@ -98,7 +98,7 @@ export default function AppNavbar({ currentUser, systemInfo, doLogout, currentUr
               {
                 hasRole(currentUser, "ROLE_USER") && (
                   <NavDropdown title="Earthquakes" id="appnavbar-earthquakes-dropdown" data-testid="appnavbar-earthquakes-dropdown" >
-                    <NavDropdown.Item href="/earthquakes/list">List Earthquakes</NavDropdown.Item>
+                    <NavDropdown.Item href="/earthquakes/list">List</NavDropdown.Item>
                     {
                       hasRole(currentUser, "ROLE_ADMIN") && (
                         <NavDropdown.Item href="/earthquakes/retrieve" data-testid="appnavbar-earthquakes-create">Create</NavDropdown.Item>
