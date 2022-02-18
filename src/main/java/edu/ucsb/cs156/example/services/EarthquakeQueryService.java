cc3bb90401da1b348557b2c097c0940c627c711d
Ryan Wenger <ryanwenger@umail.ucsb.edu>
2022-02-18 15:04:42 -0800

    rw - implemented retrieval endpoint to store earthquakes to our database

--- src/main/java/edu/ucsb/cs156/example/services/EarthquakeQueryService.java
+++ src/main/java/edu/ucsb/cs156/example/services/EarthquakeQueryService.java
@@ -1,15 +1,27 @@
 package edu.ucsb.cs156.example.services;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.extern.slf4j.Slf4j;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.springframework.web.client.RestTemplate;
+
+import edu.ucsb.cs156.example.documents.EarthquakeFeatureListing;
+import edu.ucsb.cs156.example.documents.EarthquakeFeature;
+
 import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.http.*;
+
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 import java.util.Map;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 
 @Slf4j
 @Service
@@ -25,22 +37,27 @@ public class EarthquakeQueryService {
 
     public static final String ENDPOINT = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&minmagnitude={minMag}&maxradiuskm={distance}&latitude={latitude}&longitude={longitude}";
 
-    public String getJSON(String distance, String minMag) throws HttpClientErrorException {
-        log.info("distance={}, minMag={}", distance, minMag);
+    public Iterable<EarthquakeFeature> retrieveEarthquakes(String distanceKm, String minMagnitude) throws HttpClientErrorException, JsonProcessingException {
+        log.info("distanceKm={}, minMagnitude={}", distanceKm, minMagnitude);
         HttpHeaders headers = new HttpHeaders();
         headers.setAccept(List.of(MediaType.APPLICATION_JSON));
         headers.setContentType(MediaType.APPLICATION_JSON);
 
         HttpEntity<String> entity = new HttpEntity<>(headers);
 
-        String ucsbLat = "34.4140"; // hard coded params for Storke Tower
-        String ucsbLong = "-119.8489";
-        Map<String, String> uriVariables = Map.of("minMag", minMag, "distance", distance, "latitude", ucsbLat,
+        final String ucsbLat = "34.4140"; // hard coded params for Storke Tower
+        final String ucsbLong = "-119.8489";
+        Map<String, String> uriVariables = Map.of("minMagnitude", minMagnitude, "distanceKm", distanceKm, "latitude", ucsbLat,
                 "longitude", ucsbLong);
 
         ResponseEntity<String> re = restTemplate.exchange(ENDPOINT, HttpMethod.GET, entity, String.class,
                 uriVariables);
-        return re.getBody();
+
+        String jsonResult = re.getBody();
+
+        EarthquakeFeatureListing featureListing = mapper.readValue(jsonResult, EarthquakeFeatureListing.class);
+
+        return featureListing.getFeatures();
     }
 
    
