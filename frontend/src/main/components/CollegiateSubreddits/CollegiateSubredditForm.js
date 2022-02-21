f81b84af4bfffde075d60391e0971bc11bf336bb
oliviagillam <oliviagillam@ucsb.edu>
2022-02-21 12:14:30 -0800

    og-subreddit form passing coverage tests

--- frontend/src/main/components/CollegiateSubreddits/CollegiateSubredditForm.js
+++ frontend/src/main/components/CollegiateSubreddits/CollegiateSubredditForm.js
@@ -4,7 +4,7 @@ import { useForm } from 'react-hook-form'
 import { useNavigate } from 'react-router-dom'
 
 
-function CollegiateSubredditForm({ initialUCSBDate, submitAction, buttonLabel="Create" }) {
+function CollegiateSubredditForm({ initialCollegiateSubreddit, submitAction, buttonLabel="Create" }) {
 
     // Stryker disable all
     const {
@@ -12,7 +12,7 @@ function CollegiateSubredditForm({ initialUCSBDate, submitAction, buttonLabel="C
         formState: { errors },
         handleSubmit,
     } = useForm(
-        { defaultValues: initialUCSBDate || {}, }
+        { defaultValues: initialCollegiateSubreddit || {}, }
     );
     // Stryker enable all
 
@@ -22,51 +22,35 @@ function CollegiateSubredditForm({ initialUCSBDate, submitAction, buttonLabel="C
     // Note that even this complex regex may still need some tweaks
 
     // Stryker disable next-line Regex
-    const isodate_regex = /(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+)|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d)|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d)/i;
+    //const isodate_regex = /(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+)|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d)|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d)/i;
 
     // Stryker disable next-line all
-    const yyyyq_regex = /((19)|(20))\d{2}[1-4]/i; // Accepts from 1900-2099 followed by 1-4.  Close enough.
+    //const yyyyq_regex = /((19)|(20))\d{2}[1-4]/i; // Accepts from 1900-2099 followed by 1-4.  Close enough.
 
     return (
 
         <Form onSubmit={handleSubmit(submitAction)}>
 
-            {initialUCSBDate && (
+            {(initialCollegiateSubreddit &&
                 <Form.Group className="mb-3" >
                     <Form.Label htmlFor="id">Id</Form.Label>
                     <Form.Control
-                        data-testid="UCSBDateForm-id"
+                        data-testid="CollegiateSubredditForm-id"
                         id="id"
                         type="text"
                         {...register("id")}
-                        value={initialUCSBDate.id}
+                        
+                        value={initialCollegiateSubreddit.id}
                         disabled
                     />
                 </Form.Group>
             )}
 
             <Form.Group className="mb-3" >
-                <Form.Label htmlFor="quarterYYYYQ">Quarter YYYYQ</Form.Label>
+                <Form.Label htmlFor="name">School Name</Form.Label>
                 <Form.Control
-                    data-testid="UCSBDateForm-quarterYYYYQ"
-                    id="quarterYYYYQ"
-                    type="text"
-                    isInvalid={Boolean(errors.quarterYYYYQ)}
-                    {...register("quarterYYYYQ", { required: true, pattern: yyyyq_regex })}
-                />
-                <Form.Control.Feedback type="invalid">
-                    {errors.quarterYYYYQ && 'QuarterYYYYQ is required. '}
-                    {errors.quarterYYYYQ?.type === 'pattern' && 'QuarterYYYYQ must be in the format YYYYQ, e.g. 20224 for Fall 2022'}
-                </Form.Control.Feedback>
-            </Form.Group>
-
-            <Form.Group className="mb-3" >
-                <Form.Label htmlFor="name">Name</Form.Label>
-                <Form.Control
-                    data-testid="UCSBDateForm-name"
+                    data-testid="CollegiateSubredditForm-name"
                     id="name"
-                    type="text"
-                    isInvalid={Boolean(errors.name)}
                     {...register("name", {
                         required: "Name is required."
                     })}
@@ -77,30 +61,45 @@ function CollegiateSubredditForm({ initialUCSBDate, submitAction, buttonLabel="C
             </Form.Group>
 
             <Form.Group className="mb-3" >
-                <Form.Label htmlFor="localDateTime">Date (iso format)</Form.Label>
+                <Form.Label htmlFor="location">Location</Form.Label>
                 <Form.Control
-                    data-testid="UCSBDateForm-localDateTime"
-                    id="localDateTime"
+                    data-testid="CollegiateSubredditForm-location"
+                    id="location"
                     type="text"
-                    isInvalid={Boolean(errors.localDateTime)}
-                    {...register("localDateTime", { required: true, pattern: isodate_regex })}
+                    {...register("location", {
+                        required: "Location is required."
+                    })}
                 />
                 <Form.Control.Feedback type="invalid">
-                    {errors.localDateTime && 'LocalDateTime is required. '}
-                    {errors.localDateTime?.type === 'pattern' && 'localDateTime must be in ISO format, e.g. 2022-01-02T15:30'}
+                    {errors.location?.message}
+                </Form.Control.Feedback>
+            </Form.Group>
+
+            <Form.Group className="mb-3" >
+                <Form.Label htmlFor="subreddit">Subreddit Name</Form.Label>
+                <Form.Control
+                    data-testid="CollegiateSubredditForm-subreddit"
+                    id="subreddit"
+                    type="text"
+                    {...register("subreddit", {
+                        required: "Subreddit is required."
+                    })}
+                />
+                <Form.Control.Feedback type="invalid">
+                    {errors.subreddit?.message}
                 </Form.Control.Feedback>
             </Form.Group>
 
             <Button
                 type="submit"
-                data-testid="UCSBDateForm-submit"
+                data-testid="CollegiateSubredditForm-submit"
             >
                 {buttonLabel}
             </Button>
             <Button
                 variant="Secondary"
                 onClick={() => navigate(-1)}
-                data-testid="UCSBDateForm-cancel"
+                data-testid="CollegiateSubredditForm-cancel"
             >
                 Cancel
             </Button>
@@ -110,4 +109,4 @@ function CollegiateSubredditForm({ initialUCSBDate, submitAction, buttonLabel="C
     )
 }
 
-export default UCSBDateForm;
+export default CollegiateSubredditForm;
