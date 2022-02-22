47c998c073704668b02af332789acf8fcd94b591
Elizabeth Armstrong <esarmstrong@ucsb.edu>
2022-02-22 13:24:32 -0800

    ea - finished CS Index page test full coverage

--- frontend/src/tests/pages/CollegiateSubreddits/CollegiateSubredditsIndexPage.test.js
+++ frontend/src/tests/pages/CollegiateSubreddits/CollegiateSubredditsIndexPage.test.js
@@ -1,11 +1,11 @@
-import { render } from "@testing-library/react";
+import { render, waitFor } from "@testing-library/react";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { MemoryRouter } from "react-router-dom";
 import CollegiateSubredditsIndexPage from "main/pages/CollegiateSubreddits/CollegiateSubredditsIndexPage";
 
 import { apiCurrentUserFixtures }  from "fixtures/currentUserFixtures";
 import { systemInfoFixtures } from "fixtures/systemInfoFixtures";
-import { CollegiateSubredditFixtures } from "fixtures/CollegiateSubredditFixtures";
+import { collegiateSubredditsFixtures } from "fixtures/collegiateSubredditsFixtures";
 import axios from "axios";
 import AxiosMockAdapter from "axios-mock-adapter";
 import mockConsole from "jest-mock-console";
@@ -58,6 +58,67 @@ describe("CollegiateSubredditsIndexPage tests", () => {
         );
 
 
+    });
+
+    test("renders three subreddits without crashing for regular user", async () => {
+        setupUserOnly();
+        const queryClient = new QueryClient();
+        axiosMock.onGet("/api/collegiateSubreddits/all").reply(200, collegiateSubredditsFixtures.threeSubreddits);
+
+        const { getByTestId } = render(
+            <QueryClientProvider client={queryClient}>
+                <MemoryRouter>
+                    <CollegiateSubredditsIndexPage />
+                </MemoryRouter>
+            </QueryClientProvider>
+        );
+
+        await waitFor(() => { expect(getByTestId(`${testId}-cell-row-0-col-name`)).toHaveTextContent("School"); });
+        expect(getByTestId(`${testId}-cell-row-1-col-name`)).toHaveTextContent("School2");
+        expect(getByTestId(`${testId}-cell-row-2-col-name`)).toHaveTextContent("School3");
+    });
+
+    test("renders three subreddits without crashing for admin", async () => {
+        setupAdminUser();
+        const queryClient = new QueryClient();
+        axiosMock.onGet("/api/collegiateSubreddits/all").reply(200, collegiateSubredditsFixtures.threeSubreddits);
+
+        const { getByTestId } = render(
+            <QueryClientProvider client={queryClient}>
+                <MemoryRouter>
+                    <CollegiateSubredditsIndexPage />
+                </MemoryRouter>
+            </QueryClientProvider>
+        );
+
+        await waitFor(() => { expect(getByTestId(`${testId}-cell-row-0-col-name`)).toHaveTextContent("School"); });
+        expect(getByTestId(`${testId}-cell-row-1-col-name`)).toHaveTextContent("School2");
+        expect(getByTestId(`${testId}-cell-row-2-col-name`)).toHaveTextContent("School3");
+    });
+
+    test("renders empty table when backend unavailable, user only", async () => {
+        setupUserOnly();
+
+        const queryClient = new QueryClient();
+        axiosMock.onGet("/api/collegiateSubreddits/all").timeout();
+
+        const restoreConsole = mockConsole();
+
+        const { queryByTestId } = render(
+            <QueryClientProvider client={queryClient}>
+                <MemoryRouter>
+                    <CollegiateSubredditsIndexPage />
+                </MemoryRouter>
+            </QueryClientProvider>
+        );
+
+        await waitFor(() => { expect(axiosMock.history.get.length).toBeGreaterThanOrEqual(1); });
+
+        const errorMessage = console.error.mock.calls[0][0];
+        expect(errorMessage).toMatch("Error communicating with backend via GET on /api/collegiateSubreddits/all");
+        restoreConsole();
+
+        expect(queryByTestId(`${testId}-cell-row-0-col-id`)).not.toBeInTheDocument();
     });
 
 
