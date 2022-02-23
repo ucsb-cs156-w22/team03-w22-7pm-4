b190c5da6be988e5b9296f10786147e76c9f1588
Vicky Zhou <vickyzhoujy@169-231-98-0.wireless.ucsb.edu>
2022-02-23 14:04:15 -0800

    added backend, working fine

--- frontend/src/main/pages/Earthquakes/EarthquakesIndexPage.js
+++ frontend/src/main/pages/Earthquakes/EarthquakesIndexPage.js
@@ -3,6 +3,7 @@ import { useBackend } from 'main/utils/useBackend';
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
 import EarthquakesTable from 'main/components/Earthquakes/EarthquakesTable';
 import { useCurrentUser } from 'main/utils/currentUser'
+import { Button } from 'react-bootstrap';
 export default function EarthquakesIndexPage() {
   const currentUser = useCurrentUser();
   const { data: earthquakes, error: _error, status: _status } =
@@ -12,12 +13,22 @@ export default function EarthquakesIndexPage() {
       { method: "GET", url: "/api/earthquakes/all" },
       []
     );
+  
+  // const { data: earthquakes, error: _error, status: _status } =
+  //   useBackend(
+  //     // Stryker disable next-line all : don't test internal caching of React Query
+  //     ["/api/earthquakes/purge"],
+  //     { method: "POST", url: "/api/earthquakes/purge" },
+  //     []
+  //   );
   return (
     <BasicLayout>
       <div className="pt-2">
         <h1>Earthquakes</h1>
         <EarthquakesTable earthquakes={earthquakes} currentUser={currentUser} />
+        <Button>Purge</Button>
       </div>
+
     </BasicLayout>
   )
 }
