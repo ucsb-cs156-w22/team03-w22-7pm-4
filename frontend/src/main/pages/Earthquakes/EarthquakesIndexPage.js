1ae54305669c8323f5becffa095c8aa6e6dee27e
Vicky Zhou <vickyzhoujy@Vickys-MacBook-Pro.local>
2022-02-22 20:47:09 -0800

    done with earthquake list page, 100% coverage

--- frontend/src/main/pages/Earthquakes/EarthquakesIndexPage.js
+++ frontend/src/main/pages/Earthquakes/EarthquakesIndexPage.js
@@ -1,14 +1,32 @@
+import React from 'react'
+import { useBackend } from 'main/utils/useBackend';
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
-
+import EarthquakesTable from 'main/components/Earthquakes/EarthquakesTable';
+import { useCurrentUser } from 'main/utils/currentUser'
 export default function EarthquakesIndexPage() {
+  const currentUser = useCurrentUser();
+  const { data: earthquakes, error: _error, status: _status } =
+    useBackend(
+      // Stryker disable next-line all : don't test internal caching of React Query
+      ["/api/earthquakes/all"],
+      { method: "GET", url: "/api/earthquakes/all" },
+      []
+    );
   return (
     <BasicLayout>
       <div className="pt-2">
         <h1>Earthquakes</h1>
-        <p>
-          This is where the index page will go
-        </p>
+        <EarthquakesTable earthquakes={earthquakes} currentUser={currentUser} />
       </div>
     </BasicLayout>
   )
 }
+
+
+
+
+
+
+
+
+
