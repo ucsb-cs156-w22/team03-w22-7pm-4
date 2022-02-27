90aa8f1c2952fde43508a94bb56ecb18c9fd6e5a
Vicky Zhou <vickyzhoujy@Vickys-MacBook-Pro.local>
2022-02-26 18:49:43 -0800

    added purge button and passed all tests

--- frontend/src/main/pages/Earthquakes/EarthquakesIndexPage.js
+++ frontend/src/main/pages/Earthquakes/EarthquakesIndexPage.js
@@ -1,9 +1,11 @@
 import React from 'react'
-import { useBackend } from 'main/utils/useBackend';
+import { useBackend, useBackendMutation } from 'main/utils/useBackend';
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
 import EarthquakesTable from 'main/components/Earthquakes/EarthquakesTable';
 import { useCurrentUser } from 'main/utils/currentUser'
 import { Button } from 'react-bootstrap';
+import { toast } from "react-toastify";
+
 export default function EarthquakesIndexPage() {
   const currentUser = useCurrentUser();
   const { data: earthquakes, error: _error, status: _status } =
@@ -13,20 +15,28 @@ export default function EarthquakesIndexPage() {
       { method: "GET", url: "/api/earthquakes/all" },
       []
     );
+    
+
+  function Purge_Button (){
+    const purge = useBackendMutation(
+      () => ({ url: "/api/earthquakes/purge", method: "POST" }),
+      { onSuccess: () => { toast("Successfully deleted all earthquakes!"); } },
+      // Stryker disable next-line all : don't test internal caching of React Query
+      ["/api/earthquakes/all"],
+    );
   
-  // const { data: earthquakes, error: _error, status: _status } =
-  //   useBackend(
-  //     // Stryker disable next-line all : don't test internal caching of React Query
-  //     ["/api/earthquakes/purge"],
-  //     { method: "POST", url: "/api/earthquakes/purge" },
-  //     []
-  //   );
+    return (
+      <Button onClick={ () => { purge.mutate(); } } data-testid="purge-button">
+        Purge
+      </Button>)
+  }
+
   return (
     <BasicLayout>
       <div className="pt-2">
         <h1>Earthquakes</h1>
         <EarthquakesTable earthquakes={earthquakes} currentUser={currentUser} />
-        <Button>Purge</Button>
+        <Purge_Button/>
       </div>
 
     </BasicLayout>
